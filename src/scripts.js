94dd73440848c9ff9d3b14e26a416fa519f33cbc
“Corinne <corinne.canavan@gmail.com>
2022-06-10 20:49:50 -0600

    Test Promise.All in scripts

--- src/scripts.js
+++ src/scripts.js
@@ -3,8 +3,20 @@
 
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/styles.css';
+import {allTravelersData, allTripsData, allDestinationsData, fetchData} from './apiCalls.js';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 // import './images/turing-logo.png';
 
 console.log('This is the JavaScript entry file - your code begins here.');
+
+
+
+//Promise.all
+Promise.all([allTravelersData, allTripsData, allDestinationsData])
+  .then(data => {
+    console.log("travTest", data[0].travelers);
+    console.log("tripTest", data[1].trips);
+    console.log("destinationTest", data[2].destinations);
+  })
+  .catch((error) => alert("Oops something went wrong. Try again later."));
